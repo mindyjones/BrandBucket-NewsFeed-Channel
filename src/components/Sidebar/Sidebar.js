185086fa470e0477138880a1be3de4ed5013cfde
unknown <ujjal>
2021-06-12 03:57:20 +0600

    updated finall UI

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -21,15 +21,28 @@ const Sidebar = ({ toggle, isOpen }) => {
 
                 <div className="sidebar-feedback shadow-lg bg-body rounded mt-3">
                     <h4>View Toggle</h4>
-                    <div className="toggle">
+                    <div className="toggle ">
 
-                        <div className="toggle-view d-flex justify-content-center">
-                            <div className="grid-view" onClick={toggle}>
+                        <div className="toggle-view d-flex align-items-center">
+                            <div className={
+                                isOpen
+                                ? "grid-view grid-view-tog"
+                                :"grid-view"
+                            }
+
+                            
+                            onClick={toggle}>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                                 </svg>
                             </div>
-                            <div className="tiles-view" onClick={toggle}>
+                            <div className={
+                                !isOpen
+                                ? "grid-view grid-view-tog"
+                                :"tiles-view"
+                            }
+                            
+                            onClick={toggle}>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
                                 </svg>
